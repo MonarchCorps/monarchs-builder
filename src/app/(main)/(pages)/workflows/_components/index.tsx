ef6bc1d42bad93ba678b80189f1bc3129b8ba4b4
MonarchCorps <monarchcorps01@gmail.com>
2025-07-26 00:53:05 +0100

    5

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Workflow from './workflow'
-import { onGetWorkflows } from '../_actions/workflow-connections'
 import MoreCredits from './more-creadits'
+
+// Force dynamic rendering to avoid build-time execution
+export const dynamic = 'force-dynamic'
 
 type Props = {}
 
 const Workflows = async (props: Props) => {
+  // Import function dynamically to avoid build-time execution
+  const { onGetWorkflows } = await import('../_actions/workflow-connections')
   const workflows = await onGetWorkflows()
   return (
     <div className="relative flex flex-col gap-4">
